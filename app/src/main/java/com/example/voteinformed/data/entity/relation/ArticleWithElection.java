a861990259ddb97fa9d79386a32bd129d85ce5b3
labooa <kwonmy02@gmail.com>
2025-12-02 19:08:45 -0500

    With whatever

--- app/src/main/java/com/example/voteinformed/data/entity/relation/ArticleWithElection.java
+++ app/src/main/java/com/example/voteinformed/data/entity/relation/ArticleWithElection.java
@@ -4,7 +4,7 @@ import androidx.room.Embedded;
 import androidx.room.Junction;
 import androidx.room.Relation;
 
-import com.example.voteinformed.data.entity.Acticle;
+import com.example.voteinformed.data.entity.Article;
 import com.example.voteinformed.data.entity.Election;
 
 import java.util.List;
@@ -19,5 +19,5 @@ public class ArticleWithElection {
             entityColumn = "election_id",
             associateBy = @Junction(Article_Election.class)
     )
-    public List<Election> electionss; // idk what to put
+    public List<Election> electionss;
 }
--- app/src/main/java/com/example/voteinformed/data/entity/relation/ArticleWithIssue.java
+++ app/src/main/java/com/example/voteinformed/data/entity/relation/ArticleWithIssue.java
@@ -4,7 +4,7 @@ import androidx.room.Embedded;
 import androidx.room.Junction;
 import androidx.room.Relation;
 
-import com.example.voteinformed.data.entity.Acticle;
+import com.example.voteinformed.data.entity.Article;
 import com.example.voteinformed.data.entity.Issue;
 
 import java.util.List;
